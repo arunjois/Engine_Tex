7d8a0173a00d646d3aeeef938db832bec961cfee
Arun Jois <arun.joiss@gmail.com>
2019-02-14 11:00:14 +0530

    I don't even know what I commited just now.

--- Doc.ts
+++ Doc.ts
@@ -1,5 +1,9 @@
 export class Word {
 	word:String;
+	italic:boolean=false;
+	bold:boolean=false;
+	underline:boolean=false;
+
 	constructor(w:String) {
 		this.word=w;
 	}
@@ -7,4 +11,26 @@ export class Word {
 class Paragraph extends Word {
 	alignment:any;
 
+}
+class Enumerate extends Word {
+
+}
+
+class Itemize extends Word {
+	
+}
+
+
+class Title {
+	title:Word;
+	branch:Word;
+	name:Word;
+	usn:Word;
+	guide:Word;
+	college:Word;
+	logo:String;	/* This will be removed  */
+	next:any;
+	constructor(){
+
+	}
 }
---
+++ List.ts
@@ -0,0 +1,16 @@
+class Node {
+    element:any;
+    next:any;
+    constructor(element:any){
+        this.element=element;
+    }
+}
+class List<T>{
+    head=null;
+    len=0;
+    public append(elem) {
+        current:
+
+    }
+
+}
